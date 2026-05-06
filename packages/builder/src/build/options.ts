e898f109a2321d0a3cfd718b007085052c29544e
lukekania <lukes.work@icloud.com>
2026-05-06 13:07:53 +0200

    fix(builder): dedup `define` schema entry after rebase on main

--- packages/builder/src/build/options.ts
+++ packages/builder/src/build/options.ts
@@ -31,7 +31,6 @@ export interface ApplicationOptions extends json.JsonObject {
   preserveSymlinks: boolean | null;
   statsJson: boolean | null;
   budgets: json.JsonArray | null;
-  define: json.JsonObject | null;
   externalDependencies: string[] | null;
   allowedCommonJsDependencies: string[] | null;
   extractLicenses: boolean | null;
